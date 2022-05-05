f2b0363c92cf2830ab691a4417c0a320b201745f
Yuliya Bialova <yuliya.bialova@gmail.com>
2022-05-05 11:48:30 +0300

    US-007: added ability to re-select heroes

--- src/app/hero-selection-page/hero-item/hero-item.component.ts
+++ src/app/hero-selection-page/hero-item/hero-item.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { HeroSelectionService } from "../../shared/services/hero-selecton.service";
 import { HeroItem } from "../../shared/app.interfaces";
 
@@ -6,16 +6,22 @@ import { HeroItem } from "../../shared/app.interfaces";
   selector: 'app-hero-item',
   templateUrl: './hero-item.component.html',
   styleUrls: ['./hero-item.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroItemComponent {
   @Input() heroItem!: HeroItem;
   @Output() onDelete: EventEmitter<void> = new EventEmitter();
 
-  constructor(
-    private _heroSelectionService: HeroSelectionService
-  )
-  { }
+  public get selectedHero(): HeroItem | void{
+    if (!this._heroSelectionService.selectedHero) {
+      return;
+    }
+
+    if (this._heroSelectionService.selectedHero.id === this.heroItem.id)  {
+      return this._heroSelectionService.selectedHero;
+    }
+  }
+
+  constructor(private _heroSelectionService: HeroSelectionService) { }
 
   public select(): void {
     this.heroItem.isSelected = true;
@@ -23,8 +29,17 @@ export class HeroItemComponent {
   }
 
   public delete(): void {
+    this.heroItem.isSelected = false;
     this._heroSelectionService.deleteHero(this.heroItem);
     this.onDelete.emit();
   }
 
+  public toggleSelection(): void {
+    if (this.selectedHero) {
+      return;
+    }
+
+    this._heroSelectionService.selectedHero = this.heroItem;
+    this._heroSelectionService.saveToLocalStorage();
+  }
 }
--- src/app/user-info-page/heroes-list/heroes-list.component.ts
+++ src/app/user-info-page/heroes-list/heroes-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { HeroSelectionService } from "../../shared/services/hero-selecton.service";
 import { HeroItem } from "../../shared/app.interfaces";
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
-  styleUrls: ['./heroes-list.component.css']
+  styleUrls: ['./heroes-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroesListComponent implements OnInit {
 
   public ownedHeroes: HeroItem[] = [];
 
-  constructor(private _heroSelectionService: HeroSelectionService) { }
+  constructor(
+    private _heroSelectionService: HeroSelectionService
+  ) { }
 
   public ngOnInit(): void {
     this.renderHeroesList();
